2f0bfe4965259c9234d5d3e0c750b06db445f752
needmor <sashaneedmor@gmail.com>
2026-05-08 18:02:25 +0300

    finalised design and added dynamic wirk list

---
+++ js/projects - Copy.js
@@ -0,0 +1,85 @@
+const PROJECTS = [
+  {
+    id: 1,
+
+    slug: "yandex-go-serbia",
+    title: "Yandex Go Serbia",
+
+    poster: "assets/works/project_1/img.jpg",
+
+    previewWebm: "assets/works/project_1/vid.webm",
+    previewMp4: "assets/works/project_1/vid.mp4",
+
+    vimeoId: "123456789"
+  },
+
+  {
+    id: 2,
+
+    slug: "mastercard",
+    title: "Mastercard",
+
+    poster: "assets/works/project_2/img.jpg",
+
+    previewWebm: "assets/works/project_2/vid.webm",
+    previewMp4: "assets/works/project_2/vid.mp4",
+
+    vimeoId: "987654321"
+  },
+
+  {
+    id: 3,
+
+    slug: "kia",
+    title: "KIA",
+
+    poster: "assets/works/project_3/img.jpg",
+
+    previewWebm: "assets/works/project_3/vid.webm",
+    previewMp4: "assets/works/project_3/vid.mp4",
+
+    vimeoId: "555555555"
+  },
+
+  {
+    id: 4,
+
+    slug: "yandex-go-charge",
+    title: "Yandex Go",
+
+    poster: "assets/works/project_4/img.jpg",
+
+    previewWebm: "assets/works/project_4/vid.webm",
+    previewMp4: "assets/works/project_4/vid.mp4",
+
+    vimeoId: "444444444"
+  },
+
+  {
+    id: 5,
+
+    slug: "chanel",
+    title: "Chanel",
+
+    poster: "assets/works/project_5/img.jpg",
+
+    previewWebm: "assets/works/project_5/vid.webm",
+    previewMp4: "assets/works/project_5/vid.mp4",
+
+    vimeoId: "222222222"
+  },
+
+  {
+    id: 6,
+
+    slug: "nike-run",
+    title: "Nike Run",
+
+    poster: "assets/works/project_6/img.jpg",
+
+    previewWebm: "assets/works/project_6/vid.webm",
+    previewMp4: "assets/works/project_6/vid.mp4",
+
+    vimeoId: "666666666"
+  },
+];
---
+++ js/projects.js
@@ -0,0 +1,228 @@
+const PROJECTS = [
+  {
+    id: 1,
+
+    slug: "yandex-go-serbia",
+    title: "Yandex Go Serbia",
+
+    client: "Yandex Go",
+    name: "Yandex Go Serbia",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/1_ya_serb/img.jpg",
+
+    previewWebm: "assets/works/1_ya_serb/vid.webm",
+    previewMp4: "assets/works/1_ya_serb/vid.mp4",
+
+    vimeoId: "123456789"
+  },
+
+  {
+    id: 2,
+
+    slug: "mastercard",
+    title: "Mastercard",
+
+    client: "Mastercard",
+    name: "Mastercard",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/2_mastercard/img.jpg",
+
+    previewWebm: "assets/works/2_mastercard/vid.webm",
+    previewMp4: "assets/works/2_mastercard/vid.mp4",
+
+    vimeoId: "987654321"
+  },
+
+  {
+    id: 3,
+
+    slug: "kia",
+    title: "KIA",
+
+    client: "KIA",
+    name: "KIA",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/3_KIA/img.jpg",
+
+    previewWebm: "assets/works/3_KIA/vid.webm",
+    previewMp4: "assets/works/3_KIA/vid.mp4",
+
+    vimeoId: "555555555"
+  },
+
+  {
+    id: 4,
+
+    slug: "yandex-go-charge",
+    title: "Yandex Go Charge",
+
+    client: "Yandex Go",
+    name: "Yandex Go",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/4_ya_charge/img.jpg",
+
+    previewWebm: "assets/works/4_ya_charge/vid.webm",
+    previewMp4: "assets/works/4_ya_charge/vid.mp4",
+
+    vimeoId: "444444444"
+  },
+
+  {
+    id: 5,
+
+    slug: "chanel",
+    title: "Chanel",
+
+    client: "Chanel",
+    name: "Chanel",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/5_chanel/img.jpg",
+
+    previewWebm: "assets/works/5_chanel/vid.webm",
+    previewMp4: "assets/works/5_chanel/vid.mp4",
+
+    vimeoId: "222222222"
+  },
+    {
+    id: 6,
+
+    slug: "x5-taste-and-benefits",
+    title: "X5 Retail Group",
+
+    client: "X5 Retail Group",
+    name: "Taste and Benefits",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/6_X5_Group/img.jpg",
+
+    previewWebm: "assets/works/6_X5_Group/vid.webm",
+    previewMp4: "assets/works/6_X5_Group/vid.mp4",
+
+    vimeoId: "666666666"
+  },
+
+  {
+    id: 7,
+
+    slug: "yandex-pay",
+    title: "Yandex Pay",
+
+    client: "Yandex",
+    name: "Yandex Pay",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/7_yandex_pay/img.jpg",
+
+    previewWebm: "assets/works/7_yandex_pay/vid.webm",
+    previewMp4: "assets/works/7_yandex_pay/vid.mp4",
+
+    vimeoId: "777777777"
+  },
+
+  {
+    id: 8,
+
+    slug: "rostelecom-tariff-constructor",
+    title: "Rostelecom",
+
+    client: "Rostelecom",
+    name: "Tariff Constructor",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/8_3d_Rostelecom/img.jpg",
+
+    previewWebm: "assets/works/8_3d_Rostelecom/vid.webm",
+    previewMp4: "assets/works/8_3d_Rostelecom/vid.mp4",
+
+    vimeoId: "888888888"
+  },
+
+  {
+    id: 9,
+
+    slug: "magnit",
+    title: "Magnit",
+
+    client: "Magnit",
+    name: "Magnit",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/9_magnit/img.jpg",
+
+    previewWebm: "assets/works/9_3d_Magnit/vid.webm",
+    previewMp4: "assets/works/9_3d_Magnit/vid.mp4",
+
+    vimeoId: "999999999"
+  },
+
+  {
+    id: 10,
+
+    slug: "loreal-molecular-restoration",
+    title: "L'Oréal",
+
+    client: "L'Oréal",
+    name: "Molecular Restoration",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/10_3d_Loreal/img.jpg",
+
+    previewWebm: "assets/works/10_3d_Loreal/vid.webm",
+    previewMp4: "assets/works/10_3d_Loreal/vid.mp4",
+
+    vimeoId: "101010101"
+  },
+
+  {
+    id: 11,
+
+    slug: "bork",
+    title: "BORK",
+
+    client: "BORK",
+    name: "BORK",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/11_3d_BORK/img.jpg",
+
+    previewWebm: "assets/works/11_3d_BORK/vid.webm",
+    previewMp4: "assets/works/11_3d_BORK/vid.mp4",
+
+    vimeoId: "111111111"
+  },
+
+  {
+    id: 12,
+
+    slug: "real",
+    title: "AI Production",
+
+    client: "real",
+    name: "real",
+    director: "",
+    scope: ["Creative Direction", "AI Production", "VFX", "Postproduction"],
+
+    poster: "assets/works/14_3d_real_2026/img.jpg",
+
+    previewWebm: "assets/works/14_3d_real_2026/vid.webm",
+    previewMp4: "assets/works/14_3d_real_2026/vid.mp4",
+
+    vimeoId: "121212121"
+  },
+];
